4d53a6c4e28701daba64501f3b0902ff0d7e3867
vred <artcore02@gmail.com>
2021-04-20 01:52:01 +0600

    fix CTA button link

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
         <li>Логотип на обратной стороне</li>
       </ul>
     </ol>
-    <a class="cta-button" href="mailto:dotilazarchuk@gmail.com">
+    <a class="cta-button" target="blank" href="mailto:dotilazarchuk@gmail.com?subject=%D0%A5%D0%BE%D1%87%D1%83%20%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%B8%20%D1%81%20%D0%BC%D0%BE%D0%B8%D0%BC%20%D0%B4%D0%B8%D0%B7%D0%B0%D0%B9%D0%BD%D0%BE%D0%BC%21&body=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5.%0A%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B7%D0%BE%D0%B2%D1%83%D1%82%20_%0A%D0%9C%D0%B5%D0%BD%D1%8F%20%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B5%D1%81%D1%83%D0%B5%D1%82%20%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D1%81%D1%82%D0%B2%D0%BE%20%D0%B7%D0%BD%D0%B0%D1%87%D0%BA%D0%BE%D0%B2.">
     Оставить заявку
     </a>
   </div>
